cb880ccf59cc248a9d8a4094f623e4b8592c2d7e
ax0f0f <achrafnyd@gmail.com>
2025-12-12 02:54:57 +0100

    Update user mention responses in bl-remove.js

--- cmds/bl-remove.js
+++ cmds/bl-remove.js
@@ -19,20 +19,20 @@ module.exports = {
 
     const user = message.mentions.users.first();
     if (!user) {
-      return sendReply('<:traverser:1400313375547850877> Please mention a user to remove from your blacklist.');
+      return sendReply('<:discotoolsxyzicon:1448758684535488562> Please mention a user to remove from your blacklist.');
     }
 
     db.get(`SELECT * FROM blacklist_users WHERE owner_id = ? AND blacklisted_id = ? AND guild_id = ?`, [ownerId, user.id, guildId], (err, row) => {
-      if (err) return sendReply('<:traverser:1400313375547850877> Database error.');
+      if (err) return sendReply('<:discotoolsxyzicon:1448758684535488562> Database error.');
 
       if (!row) {
-        return sendReply(`⚠️ <@${user.id}> is not in your blacklist.`);
+        return sendReply(`<:warn1:1448792086810726601> <@${user.id}> is not in your blacklist.`);
       }
 
       db.run(`DELETE FROM blacklist_users WHERE owner_id = ? AND blacklisted_id = ? AND guild_id = ?`, [ownerId, user.id, guildId], (err) => {
-        if (err) return sendReply('<:traverser:1400313375547850877> Failed to remove from blacklist.');
+        if (err) return sendReply('<:discotoolsxyzicon:1448758684535488562> Failed to remove from blacklist.');
 
-        sendReply(`<:verifier:1400313376521064551> You have removed <@${user.id}> from your blacklist.`);
+        sendReply(`<:discotoolsxyzicon1:1448758665963110603> You have removed <@${user.id}> from your blacklist.`);
       });
     });
   }
