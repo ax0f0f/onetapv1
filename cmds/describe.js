c2778489ed7c048b2f0cccce4d515691f43f8da1
ax0f0f <achrafnyd@gmail.com>
2025-12-12 02:57:36 +0100

    Update bot announcement and footer details

--- cmds/describe.js
+++ cmds/describe.js
@@ -19,10 +19,10 @@ module.exports = {
             });
         }
 
-        const titleText = new TextDisplayBuilder().setContent(`## @everyone @here📢 Bot Announcement: ax0f is Now Public!`);
-        const introText = new TextDisplayBuilder().setContent(`> Great news! **ax0f** is now available for everyone to use.`);
+        const titleText = new TextDisplayBuilder().setContent(`## @everyone @here  <:reportt:1448797160614133940> Bot Announcement: Pivot is Now Public!`);
+        const introText = new TextDisplayBuilder().setContent(`> Great news! **Pivot** is now available for everyone to use.`);
         const featuresTitle = new TextDisplayBuilder().setContent(`### What I do:`);
-        const featuresText = new TextDisplayBuilder().setContent(`I create temporary voice and text channels instantly. Just join the **"One Tap"** voice channel, and I'll give you a private room. As the room owner, you can use the control panel in the **#interface** channel to manage it.`);
+        const featuresText = new TextDisplayBuilder().setContent(`I create temporary voice and text channels instantly. Just join the **"One Tap"** voice channel, and I'll give you a private room. As the room owner, you can use the control panel to manage it.`);
         const styleTitle = new TextDisplayBuilder().setContent(`### New Look:`);
         const styleText = new TextDisplayBuilder().setContent(`I've been updated with a fresh, new visual style for all my messages to provide a cleaner and more intuitive experience.`);
         const commandsTitle = new TextDisplayBuilder().setContent(`### Available Commands:`);
@@ -31,7 +31,7 @@ module.exports = {
         const managerCommands = new TextDisplayBuilder().setContent(`- **Managers:** \`man-add\`, \`man-remove\`, \`man-clear\``);
         const whitelistCommands = new TextDisplayBuilder().setContent(`- **Whitelist:** \`wl-add\`, \`wl-remove\`, \`wl-list\``);
         const blacklistCommands = new TextDisplayBuilder().setContent(`- **Blacklist:** \`bl-add\`, \`bl-remove\`, \`bl-list\``);
-        const footerText = new TextDisplayBuilder().setContent(`[Developed by ichbi9o](https://discord.gg/hmm7u4rCrk)`);
+        const footerText = new TextDisplayBuilder().setContent(`[Developed by ax0f](https://discord.gg/hmm7u4rCrk)`);
 
         const container = new ContainerBuilder()
             .addTextDisplayComponents(
