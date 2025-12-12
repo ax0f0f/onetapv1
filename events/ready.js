1c81fcb5945c7d733d479dc96407c07616a95f7f
ax0f0f <achrafnyd@gmail.com>
2025-12-12 02:50:51 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,21 @@
 module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-      console.log(`✅ Logged in as ${client.user.tag}`);
-    }
-  };
-  
+  name: "ready",
+  once: true,
+  execute(client) {
+
+    console.log(`✅ Logged in as ${client.user.tag}`);
+
+    // ⭕ Your custom presence
+    client.user.setPresence({
+      activities: [
+        {
+          name: ".v help | Made in Heaven By AxOf", 
+          type: 3 
+        }
+      ],
+      status: "dnd" 
+    });
+
+    console.log("👀 Presence set: Watching .v help | Made in Heaven By AxOf (DND)");
+  }
+};
